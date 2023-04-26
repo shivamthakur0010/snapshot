4a5436948b8a65d7be08acfae8804dd238e91c19
shivamthakur0010 <demonking0010@gmail.com>
2023-04-26 22:45:33 +0530

    Updates

---
+++ precache-manifest.e4b2a08ca7e094b1c4aded6da650d9ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a55da0bef5268dfdd85a1345bd12be8",
+    "url": "/snapshot/index.html"
+  },
+  {
+    "revision": "1e7cd5fda20398143281",
+    "url": "/snapshot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "233109568c205310a18b",
+    "url": "/snapshot/static/js/2.1b86b8b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snapshot/static/js/2.1b86b8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7cd5fda20398143281",
+    "url": "/snapshot/static/js/main.e0acb705.chunk.js"
+  },
+  {
+    "revision": "4d6d0105be043c70fb56",
+    "url": "/snapshot/static/js/runtime-main.5d3f71fe.js"
+  }
+]);
